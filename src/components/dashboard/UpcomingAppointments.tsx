1126f0a0a2d017857e0f0dcc134b59dc4e5499ce
htabir <htabir.me@gmail.com>
2022-12-19 22:02:39 +0600

    ADDED: visits of period card added, populated upcoming appointments with image

--- src/components/dashboard/UpcomingAppointments.tsx
+++ src/components/dashboard/UpcomingAppointments.tsx
@@ -3,6 +3,40 @@ import {Avatar, Grid, Typography} from "@mui/material";
 import Box from "@mui/material/Box";
 import theme from "../../theme";
 
+interface AppointmentCardProps {
+    name: string;
+    time: string;
+    avatar: string;
+}
+
+const AppointmentCard: React.FC<AppointmentCardProps> = ({name, time, avatar}) => {
+    return (
+        <Grid item xs={12}>
+            <Box
+                sx={{
+                    height: 56,
+                    bgcolor: theme.palette.primary.dark,
+                    borderRadius: 4,
+                    display: 'flex',
+                    alignItems: 'center',
+                    paddingX: 2,
+                }}
+            >
+                <Typography variant={"body1"} sx={{color: "white", width: 96}}>{time}</Typography>
+                <Avatar
+                    sx={{
+                        height: 40, width: 40
+                    }}
+                    src={avatar}
+                >
+
+                </Avatar>
+                <Typography variant={"body1"} sx={{color: "white", ml: 2}}>{name}</Typography>
+            </Box>
+        </Grid>
+    );
+}
+
 const UpcomingAppointments = () => {
     return (
         <Box
@@ -17,50 +51,14 @@ const UpcomingAppointments = () => {
         >
             <Typography variant={"body1"} sx={{mb: 2, fontWeight: 600}}>Upcoming Appointments</Typography>
             <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Box
-                        sx={{
-                            height: 56,
-                            bgcolor: theme.palette.primary.dark,
-                            borderRadius: 4,
-                            display: 'flex',
-                            alignItems: 'center',
-                            paddingX: 2,
-                        }}
-                    >
-                        <Typography variant={"body1"} sx={{color: "white", width: 96}}>08:30 am</Typography>
-                        <Avatar
-                            sx={{
-                                height: 40, width: 40
-                            }}
-                        >
-                            I
-                        </Avatar>
-                        <Typography variant={"body1"} sx={{color: "white", ml: 2}}>Ivan</Typography>
-                    </Box>
-                </Grid>
-                <Grid item xs={12}>
-                    <Box
-                        sx={{
-                            height: 56,
-                            bgcolor: theme.palette.primary.dark,
-                            borderRadius: 4,
-                            display: 'flex',
-                            alignItems: 'center',
-                            paddingX: 2,
-                        }}
-                    >
-                        <Typography variant={"body1"} sx={{color: "white", width: 96}}>08:30 am</Typography>
-                        <Avatar
-                            sx={{
-                                height: 40, width: 40
-                            }}
-                        >
-                            I
-                        </Avatar>
-                        <Typography variant={"body1"} sx={{color: "white", ml: 2}}>Ivan</Typography>
-                    </Box>
-                </Grid>
+
+                <AppointmentCard name={"Ivan"} time={"08:30 AM"}
+                                 avatar={"https://cdn-icons-png.flaticon.com/512/147/147133.png"}/>
+                <AppointmentCard name={"Arter"} time={"09:30 AM"}
+                                 avatar={"https://cdn-icons-png.flaticon.com/512/147/147142.png"}/>
+                <AppointmentCard name={"Lorem"} time={"09:40 AM"}
+                                 avatar={"https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small_2x/beautiful-latin-woman-avatar-character-icon-free-vector.jpg"}/>
+
             </Grid>
         </Box>
     );
--- src/components/dashboard/VisitsOfPeriodsCard.tsx
+++ src/components/dashboard/VisitsOfPeriodsCard.tsx
@@ -3,6 +3,67 @@ import theme from "../../theme";
 import {Typography} from "@mui/material";
 import Box from "@mui/material/Box";
 import ChartLabel from "./ChartLabel";
+import {
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+} from 'chart.js';
+import {Line} from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    tension: 0.4,
+    plugins: {
+        legend: {
+            display: false
+        },
+    },
+    scales: {
+        x: {
+            grid: {
+                display: false,
+            }
+        },
+        y: {
+            grid: {
+                display: false,
+            }
+        }
+    }
+};
+
+const labels = ['10', '11', '12', '13', '14', '15', '16'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            data: [10, 30, 80, 41, 56, 55, 40],
+            borderColor: theme.palette.primary.main,
+            pointRadius: 0,
+        },
+        {
+            data: [28, 48, 40, 19, 86, 27, 90],
+            borderColor: theme.palette.secondary.main,
+            pointRadius: 0,
+        },
+    ],
+};
 
 const VisitsOfPeriodsCard = () => {
     return (
@@ -21,9 +82,11 @@ const VisitsOfPeriodsCard = () => {
                     height: 400,
                     bgcolor: theme.palette.background.default,
                     borderRadius: 8,
-                    mt: 2
+                    mt: 2,
+                    padding: 2
                 }}
             >
+                <Line options={options} data={data}/>
 
             </Box>
             <Box
@@ -34,7 +97,7 @@ const VisitsOfPeriodsCard = () => {
             >
                 <ChartLabel label={"Men"} color={theme.palette.primary.main} data={20} gap={0.15}/>
                 <Box sx={{width: 16}}/>
-                <ChartLabel label={"Men"} color={theme.palette.secondary.main} data={20} gap={0.15}/>
+                <ChartLabel label={"Women"} color={theme.palette.secondary.main} data={20} gap={0.15}/>
             </Box>
         </Box>
     );
